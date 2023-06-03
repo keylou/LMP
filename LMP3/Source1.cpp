2203ee29f811ab79ec8172e52a2d477e5596db7c
keylou <116100969+keylou@users.noreply.github.com>
2023-06-03 09:59:40 +0300

    Update Source1.cpp

--- LMP3/Source1.cpp
+++ LMP3/Source1.cpp
@@ -66,112 +66,6 @@ void task1(Tree& root)
 	}
 }
 
-void task2(Tree& p)
-{
-	Tree root = p;
-	stack st;
-	stack oper;
-
-	//пока есть дерево влево, то в стек заносится правое поддерево
-	//если в корне хранится арифметический знак, сохраняем корень
-	//обходим все дерево, потом снизу вверх переходим на корни с арифметическими знаками и делаем проверку
-
-	while (root)
-	{
-		if (root->left)
-		{
-			if (root->right)
-			{
-				st.push(root->right);
-			}
-			if (root->info == '+' || root->info == '-' || root->info == '*' || root->info == '^')
-				oper.push(root);
-			root = root->left;
-		}
-		else if (root->right)
-		{
-			if (root->info == '+' || root->info == '-' || root->info == '*' || root->info == '^')
-				oper.push(root);
-			root = root->right;
-		}
-		else if (st.empty())
-			root = nullptr;
-		else
-			root = st.pop();
-	}
-	while (!oper.empty())
-	{
-		root = oper.pop();
-		std::cout << '\n';
-		Print(root);
-		std::cout << '\n';
-		tree_formula(root);
-		if (root->info == '+' || root->info == '-')
-		{
-			if (root->right->info == '0')
-			{
-				Clear(root->right);
-				Tree ptr = root->left;
-				root = ptr;
-			}
-			else if (root->left->info == '0')
-			{
-				Clear(root->left);
-				Tree ptr = root->right;
-				root = ptr;
-			}
-			std::cout << '\n';
-			Print(root);
-		}
-		else if (root->info == '*')
-		{
-			if (root->right->info == '0' || root->left->info == '0')
-			{
-				root->info = '0';
-				Clear(root->left);
-				Clear(root->right);
-			}
-			else if (root->left->info == '1')
-			{
-				Clear(root->left);
-				Tree ptr = root->right;
-				root = ptr;
-			}
-			else if (root->right->info == '1')
-			{
-				Clear(root->right);
-				Tree ptr = root->left;
-				root = ptr;
-			}
-			std::cout << '\n';
-			Print(root);
-		}
-		else if (root->info == '^')
-		{
-			if (root->right->info == '0' || root->left->info == '0')
-			{
-				root->info = '1';
-				Clear(root->left);
-				Clear(root->right);
-			}
-			else if (root->left->info == '1')
-			{
-				Clear(root->left);
-				Tree ptr = root->right;
-				root = ptr;
-			}
-			else if (root->right->info == '1')
-			{
-				Clear(root->right);
-				Tree ptr = root->left;
-				root = ptr;
-			}
-			std::cout << '\n';
-			Print(root);
-		}
-	}
-}
-
 void main()
 {
 	std::ifstream file("form.txt");
@@ -181,7 +75,7 @@ void main()
 
 	tree_formula(root);
 	std::cout << '\n';
-	task2(root);
+	task1(root);
 	tree_formula(root);
 
 	Clear(root);
